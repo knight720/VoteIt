a47687489b40cd0a778a814f255318baabea3b36
Knight Huang <knighthuang@91app.com>
2018-11-24 07:33:09 +0800

    Feeds API CRUD

---
+++ src/VoteIt/VoteIt/Controllers/Apis/FeedsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VoteIt.Models;
+
+namespace VoteIt.Controllers.Apis
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedsController : ControllerBase
+    {
+        private readonly VoteItDBContext _context;
+
+        public FeedsController(VoteItDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Feeds
+        [HttpGet]
+        public IEnumerable<Feed> GetFeed()
+        {
+            return _context.Feed;
+        }
+
+        // GET: api/Feeds/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFeed([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var feed = await _context.Feed.FindAsync(id);
+
+            if (feed == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(feed);
+        }
+
+        // PUT: api/Feeds/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFeed([FromRoute] int id, [FromBody] Feed feed)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != feed.FeedId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(feed).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FeedExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Feeds
+        [HttpPost]
+        public async Task<IActionResult> PostFeed([FromBody] Feed feed)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Feed.Add(feed);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFeed", new { id = feed.FeedId }, feed);
+        }
+
+        // DELETE: api/Feeds/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFeed([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var feed = await _context.Feed.FindAsync(id);
+            if (feed == null)
+            {
+                return NotFound();
+            }
+
+            _context.Feed.Remove(feed);
+            await _context.SaveChangesAsync();
+
+            return Ok(feed);
+        }
+
+        private bool FeedExists(int id)
+        {
+            return _context.Feed.Any(e => e.FeedId == id);
+        }
+    }
+}
