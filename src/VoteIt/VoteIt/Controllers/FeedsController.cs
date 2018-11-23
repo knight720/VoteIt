1edf1fcd3c770262793c732324a8532be55d6d19
Knight Huang <knighthuang@91app.com>
2018-11-24 01:32:48 +0800

    DbContextPool, CRUD Page

---
+++ src/VoteIt/VoteIt/Controllers/FeedsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VoteIt.Models;
+
+namespace VoteIt.Controllers
+{
+    public class FeedsController : Controller
+    {
+        private readonly VoteItDBContext _context;
+
+        public FeedsController(VoteItDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Feeds
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Feed.ToListAsync());
+        }
+
+        // GET: Feeds/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var feed = await _context.Feed
+                .FirstOrDefaultAsync(m => m.FeedId == id);
+            if (feed == null)
+            {
+                return NotFound();
+            }
+
+            return View(feed);
+        }
+
+        // GET: Feeds/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Feeds/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FeedId,FeedTitle,FeedLike,FeedCreatedDateTime,FeedCreatedUser,FeedValidFlag")] Feed feed)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(feed);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(feed);
+        }
+
+        // GET: Feeds/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var feed = await _context.Feed.FindAsync(id);
+            if (feed == null)
+            {
+                return NotFound();
+            }
+            return View(feed);
+        }
+
+        // POST: Feeds/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FeedId,FeedTitle,FeedLike,FeedCreatedDateTime,FeedCreatedUser,FeedValidFlag")] Feed feed)
+        {
+            if (id != feed.FeedId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(feed);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FeedExists(feed.FeedId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(feed);
+        }
+
+        // GET: Feeds/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var feed = await _context.Feed
+                .FirstOrDefaultAsync(m => m.FeedId == id);
+            if (feed == null)
+            {
+                return NotFound();
+            }
+
+            return View(feed);
+        }
+
+        // POST: Feeds/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var feed = await _context.Feed.FindAsync(id);
+            _context.Feed.Remove(feed);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FeedExists(int id)
+        {
+            return _context.Feed.Any(e => e.FeedId == id);
+        }
+    }
+}
--- src/VoteIt/VoteIt/Startup.cs
+++ src/VoteIt/VoteIt/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using VoteIt.Models;
 
 namespace VoteIt
 {
@@ -33,6 +35,14 @@ namespace VoteIt
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddEntityFrameworkSqlServer();
+
+            services.AddDbContextPool<VoteItDBContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("VoteItDBDatabase")));
+
+            var option = new DbContextOptionsBuilder().UseSqlServer(Configuration.GetConnectionString("VoteItDBDatabase")).Options;
+            services.AddSingleton(option).AddScoped<VoteItDBContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
