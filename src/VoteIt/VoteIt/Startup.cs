7bd89a24217670fe088c8d90467d2c8f046a5b0b
Knight Huang <knighthuang@91app.com>
2018-11-28 00:25:02 +0800

    使用 OAuth

--- src/VoteIt/VoteIt/Startup.cs
+++ src/VoteIt/VoteIt/Startup.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -34,18 +30,30 @@ namespace VoteIt
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddEntityFrameworkSqlServer();
 
+            //// General
             services.AddScoped<FeedRepository>();
 
-            services.AddDbContextPool<VoteItDBContext>(options => 
+            //// Database
+            services.AddDbContextPool<VoteItDBContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("VoteItDBDatabase")));
 
             var option = new DbContextOptionsBuilder().UseSqlServer(Configuration.GetConnectionString("VoteItDBDatabase")).Options;
             services.AddSingleton(option).AddScoped<VoteItDBContext>();
+
+            //// Auth
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthentication().AddGoogle(googleOptions =>
+            {
+                googleOptions.ClientId = Configuration["Authentication:Google:ClientId"];
+                googleOptions.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
