8873f1128b0d322c7a466ef78e33ea54d2f424e9
Knight Huang <knighthuang@91app.com>
2019-05-22 09:30:25 +0800

    Add appsettings.json.user

---
+++ src/VoteIt/VoteIt/Controllers/Apis/EnvironmentController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace VoteIt.Controllers.Apis
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnvironmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public EnvironmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            Dictionary<string, string> environment = new Dictionary<string, string>
+            {
+                {"Tag",  _configuration["Tag"]},
+            };
+
+            return new JsonResult(environment);
+        }
+    }
+}
--- src/VoteIt/VoteIt/Program.cs
+++ src/VoteIt/VoteIt/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace VoteIt
 {
@@ -19,6 +13,10 @@ namespace VoteIt
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.json.user", true, true);
+                })
                 .UseStartup<Startup>();
     }
 }
